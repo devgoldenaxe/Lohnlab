95e49ba69cb89a8ac4950aab8634588d7ab29f9e
sabareeswaran <151923697+devgoldenaxe@users.noreply.github.com>
2024-08-09 10:24:49 +0530

    files deleted

--- module2.js
+++ module2.js
@@ -19,219 +19,282 @@ let Werbung = 21;
 let garage = 40;
 let Fehlgeld = 16;
 
-const data = [
+let data = [
   {
     "AN PersNr": "1",
     "AN Nachname": "Nicole",
     "brutto Gehalt": 3500,
     Steuerklasse: 1,
     Uberstunden: 0,
+    R: 0,
+    AL: 0,
+    Z: 0,
+    T: 0,
+    AN: 0,
+    BM: 0,
+    Y: 0,
+    AX: 0,
+    AP: 0,
+    AR: 0,
+    AT: 0,
+    X: 0,
+    BT: 0,
+    BC: 0,
+    S: "ja",
+    V: 0,
+    W: "ja",
+    AB: "nein",
+    AF: 0,
+    U: 0,
+    BI: 0,
+    CC: 0,
+    Q: 0,
+    AD: 0,
   },
 ];
 
-for (let bsi = 0; bsi < BSlist.length; bsi++) {
-  processBS(BSlist[bsi]);
-}
-
 function processBS(BS) {
-  let gesamt = 0;
-  for (let i = 0; i < data.length; i++) {
-    gesamt = data[i]["R"];
-  }
-  let BS_result;
-  let spalte;
   let Daten = {
     T1: 7.23,
     T2: 3,
     T3: 14,
     T4: 3,
   };
-  switch (BS) {
-    case "Sachbezug":
-      BS_result = 50 - (data[i]["AL"] - data[i]["Z"]);
-      spalte = 38;
-      break;
-    case "Handy":
-      BS_result = data[i]["T"] - data[i]["AN"];
-      spalte = 40;
-      break;
-    case "Internet":
-      BS_result = 50 - data[i]["BM"];
-      spalte = 65;
-      break;
-    case "CleverLunch":
-      BS_result = data[i]["Y"] * Daten.T1 * Daten.T1 - data[i]["AX"];
-      spalte = 50;
-      break;
-    case "Werbung":
-      BS_result = 21 - data[i]["AP"];
-      spalte = 42;
-      break;
-    case "Garage":
-      BS_result = 40 - data[i]["AR"];
-      spalte = 44;
-      break;
-    case "Fehlgeld":
-      BS_result = 16 - data[i]["AT"];
-      spalte = 46;
-      break;
-    case "Fahrkosten":
-      spalte = 63;
-      break;
-    case "Kindergarten":
-      spalte = 61;
-      BS_result = data[i]["X"] - data[i]["BT"];
-      break;
-    case "Verpflegungsmehraufwand":
-      spalte = 55;
-      BS_result = data[i]["Y"] * Daten.T3 * Daten.T4 - data[i]["BC"];
-      break;
-    default:
-      for (let i = 0; i < data.length; i++) {
-        if (data[i]["AF"] > 0 && data[i]["U"] > 0) {
-          if (data[i]["AF"] > data[i]["U"]) {
-            data[i]["BI"] = data[i]["U"];
-            data[i]["AF"] -= data[i]["BI"];
-          } else {
-            data[i]["BI"] = data[i]["AF"];
-            data[i]["AF"] = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    let gesamt = data[i]["R"]; // Initial amount
+    let BS_result;
+    let spalte;
+
+    // Determine BS_result and spalte based on the value of BS
+    switch (BS) {
+      case "Sachbezug":
+        BS_result = 50 - (data[i]["AL"] - data[i]["Z"]);
+        spalte = 38;
+        console.log("BS_result", BS_result);
+        break;
+      case "Handy":
+        BS_result = data[i]["T"] - data[i]["AN"];
+        spalte = 40;
+        break;
+      case "Internet":
+        BS_result = 50 - data[i]["BM"];
+        spalte = 65;
+        break;
+      case "CleverLunch":
+        BS_result = data[i]["Y"] * Daten.T1 * Daten.T1 - data[i]["AX"];
+        spalte = 50;
+        break;
+      case "Werbung":
+        BS_result = 21 - data[i]["AP"];
+        spalte = 42;
+        break;
+      case "Garage":
+        BS_result = 40 - data[i]["AR"];
+        spalte = 44;
+        break;
+      case "Fehlgeld":
+        BS_result = 16 - data[i]["AT"];
+        spalte = 46;
+        break;
+      case "Fahrkosten":
+        spalte = 63;
+        break;
+      case "Kindergarten":
+        spalte = 61;
+        break;
+      case "Verpflegungsmehraufwand":
+        BS_result = data[i]["Y"] * Daten.T3 * Daten.T4 - data[i]["BC"];
+        spalte = 55;
+
+        break;
+      default:
+        for (let j = 0; j < data.length; j++) {
+          if (data[j]["AF"] > 0 && data[j]["U"] > 0) {
+            if (data[j]["AF"] > data[j]["U"]) {
+              data[j]["BI"] = data[j]["U"];
+              data[j]["AF"] -= data[j]["BI"];
+            } else {
+              data[j]["BI"] = data[j]["AF"];
+              data[j]["AF"] = 0;
+            }
           }
         }
+    }
+
+    if (BS !== "Sachbezug") {
+      if (BS !== "Internet") {
+        if (BS !== "CleverLunch") {
+          if (BS !== "Fehlgeld") {
+            if (BS !== "Handy") {
+              if (BS !== "Werbung") {
+                if (BS !== "Garage") {
+                  if (BS !== "Kindergarten") {
+                    if (BS !== "Verpflegungsmehraufwand") {
+                      if (BS === "Fahrkosten") {
+                        if (data[i]["S"] === "ja") {
+                          BS_result = 1;
+                          console.log("BS_result", BS_result);
+                        } else {
+                          let Fahrt = data[i]["V"];
+                          if (Fahrt > 20) {
+                            BS_result =
+                              20 * (data[i]["Y"] * 3) * 0.3 +
+                              (Fahrt - 20) * (data[i]["Y"] * 3) * 0.38;
+                            console.log("BS_result", BS_result);
+                          } else {
+                            BS_result = Fahrt * (data[i]["Y"] * 3) * 0.3;
+                          }
+                        }
+
+                        if (BS_result > gesamt) {
+                          BS_result = gesamt;
+                          gesamt = 0;
+                          console.log("BS_result", BS_result); // Corrected from setting BS_result to 0
+                        } else {
+                          gesamt -= BS_result;
+                        }
+
+                        return;
+                      } else {
+                        // Verpflegungsmehraufwand
+                        if (data[i]["W"] !== "ja") {
+                          BS_result = 0;
+                        }
+
+                        if (BS_result > gesamt) {
+                          BS_result = gesamt;
+                          gesamt = 0;
+                        } else {
+                          gesamt -= BS_result;
+                        }
+
+                        return;
+                      }
+                    } else {
+                      // Kindergarten
+                      if (BS_result > gesamt) {
+                        BS_result = gesamt;
+                        gesamt = 0;
+                      } else {
+                        gesamt -= BS_result;
+                      }
+
+                      return;
+                    }
+                  } else {
+                    // Garage
+                    if (data[i]["S"] !== "ja") {
+                      BS_result = 0;
+                    } else {
+                      if (BS_result > gesamt) {
+                        BS_result = gesamt;
+                        gesamt = 0;
+                        console.log("BS_result", BS_result);
+                      } else {
+                        gesamt -= BS_result;
+                      }
+                    }
+
+                    return;
+                  }
+                } else {
+                  // Werbung
+                  if (data[i]["S"] === "ja") {
+                    BS_result = 0;
+                  } else {
+                    if (BS_result > gesamt) {
+                      BS_result = gesamt;
+                      gesamt = 0;
+                    } else {
+                      gesamt -= BS_result;
+                    }
+                  }
+
+                  return;
+                }
+              } else {
+                // Handy
+                if (data[i]["Y"] !== "ja") {
+                  if (BS_result > gesamt) {
+                    BS_result = gesamt;
+                    gesamt = 0;
+                  } else {
+                    gesamt -= BS_result;
+                  }
+                } else {
+                  BS_result = 0;
+                }
+
+                return;
+              }
+            } else {
+              // Fehlgeld
+              if (data[i]["AB"] === "ja") {
+                if (BS_result > gesamt) {
+                  BS_result = gesamt;
+                  gesamt = 0;
+                } else {
+                  gesamt -= BS_result;
+                }
+              } else {
+                BS_result = 0;
+              }
+
+              return;
+            }
+          } else {
+            // CleverLunch
+            if (data[i]["W"] === "ja") {
+              BS_result = 0;
+            }
+            if (BS_result > gesamt) {
+              BS_result = gesamt;
+              gesamt = 0;
+            } else {
+              gesamt -= BS_result;
+            }
+
+            return;
+          }
+        } else {
+          // Internet
+          if (BS_result > gesamt) {
+            BS_result = gesamt;
+            gesamt = 0;
+          } else {
+            gesamt -= BS_result;
+          }
+
+          return;
+        }
+      } else {
+        // Sachbezug
+        if (BS_result > gesamt) {
+          BS_result = gesamt;
+          gesamt = 0;
+        } else {
+          gesamt -= BS_result;
+        }
+
+        return;
       }
+    } else {
+      if (BS_result > gesamt) {
+        BS_result = gesamt;
+        gesamt = 0;
+      } else {
+        gesamt -= BS_result;
+      }
+      // console.log("BS_Result", BS_result);
 
       return;
-  }
-  if (
-    ![
-      "Sachbezug",
-      "Internet",
-      "CleverLunch",
-      "Fehlgeld",
-      "Handy",
-      "Werbung",
-      "Garage",
-      "Kindergarten",
-      "Verpflegungsmehraufwand",
-    ].includes(BS)
-  ) {
-    if (BS === "Fahrkosten") {
-      if (data[i]["S"] === "ja") {
-        BS_result = 0;
-      } else {
-        let Fahrt = data[i]["V"];
-        if (Fahrt > 20) {
-          BS_result =
-            20 * (data[i]["Y"] * 3) * 0.3 +
-            (Fahrt - 20) * (data[i]["Y "] * 3) * 0.38;
-        } else {
-          BS_result = Fahrt * (data[i]["Y "] * 3) * 0.3;
-        }
-      }
-      if (BS_result > gesamt) {
-        BS_result = gesamt;
-        gesamt = 0;
-      } else {
-        gesamt -= BS_result;
-      }
-      ende1();
-    } else if (BS === "Verpflegungsmehraufwand") {
-      if (data[i]["W"] !== "ja") {
-        BS_result = 0;
-      }
-      if (BS_result > gesamt) {
-        BS_result = gesamt;
-        gesamt = 0;
-      } else {
-        gesamt -= BS_result;
-      }
-      ende1();
-    } else if (BS === "Kindergarten") {
-      if (BS_result > gesamt) {
-        BS_result = gesamt;
-        gesamt = 0;
-      } else {
-        gesamt -= BS_result;
-      }
-      ende1();
-    } else if (BS === "Garage") {
-      if (data[i]["S"] !== "ja") {
-        BS_result = 0;
-      }
-      if (BS_result > gesamt) {
-        BS_result = gesamt;
-        gesamt = 0;
-      } else {
-        gesamt -= BS_result;
-      }
-      ende1();
-    } else if (BS === "Werbung") {
-      if (data[i]["S"] === "ja") {
-        BS_result = 0;
-      }
-      if (BS_result > gesamt) {
-        BS_result = gesamt;
-        gesamt = 0;
-      } else {
-        gesamt -= BS_result;
-      }
-      ende1();
-    } else if (BS === "Handy") {
-      if (data[i]["Y"] !== "ja") {
-        if (BS_result > gesamt) {
-          BS_result = gesamt;
-          gesamt = 0;
-        } else {
-          gesamt -= BS_result;
-        }
-      } else {
-        BS_result = 0;
-      }
-      ende1();
-    } else if (BS === "Fehlgeld") {
-      if (data[i]["AB"] === "ja") {
-        if (BS_result > gesamt) {
-          BS_result = gesamt;
-          gesamt = 0;
-        } else {
-          gesamt -= BS_result;
-        }
-      } else {
-        BS_result = 0;
-      }
-      ende1();
-    } else if (BS === "CleverLunch") {
-      if (data[i]["W"] === "ja") {
-        BS_result = 0;
-      }
-      if (BS_result > gesamt) {
-        BS_result = gesamt;
-        gesamt = 0;
-      } else {
-        gesamt -= BS_result;
-      }
-      ende1();
-    } else if (BS === "Internet") {
-      if (BS_result > gesamt) {
-        BS_result = gesamt;
-        gesamt = 0;
-      } else {
-        gesamt -= BS_result;
-      }
-      ende1();
     }
-  } else if (BS === "Sachbezug") {
-    if (BS_result > gesamt) {
-      BS_result = gesamt;
-      gesamt = 0;
-    } else {
-      gesamt -= BS_result;
-    }
-    ende1();
   }
-  function ende1() {
-    data[i][[data.length][spalte - 1]] = BS_result;
-    data[i][[data.length][31]] = gesamt;
-  }
+}
+
+for (let bsi = 0; bsi < BSlist.length && bsi < 10; bsi++) {
+  processBS(BSlist[bsi], bsi);
 }
 
 for (let i = 0; i < data.length; i++) {
@@ -316,11 +379,9 @@ let F14 = Daten2.G14 * F1;
 let F15 = Daten2.G15 * F1;
 let F16 = Daten2.G16 * F1;
 
-//console.log(F16);
-
 for (let i = 0; i < data.length; i++) {
   if (data[i]["AX"] > 0) {
-    data[i]["AX"] = Math.round(data[i]["AX"] * 100) / 100;
+    data[i]["AW"] = Math.round(data[i]["AX"] * 100) / 100;
     let CL = data[i]["AX"];
 
     if (CL <= A2) {
@@ -426,7 +487,41 @@ for (let i = 0; i < data.length; i++) {
   if (data[i]["AF"] > 0) {
     data[i]["AG"] = Math.round((data[i]["AF"] / data[i]["Q"]) * 100) / 100;
   }
-  if (data[i]["cc"] > 0) {
-    data[i]["AD"] = ((data[i]["CC"] - data[i]["AG"]) * data[5]["AD"]) / 100;
+
+  if (data[i]["CC"] > 0) {
+    data[i]["AD"] = ((data[i]["CC"] - data[i]["AG"]) * data[i]["AD"]) / 100;
   }
+  console.log("CL", CL);
+
+  console.log(data);
+  console.log(A2);
+  console.log(A3);
+  console.log(A4);
+  console.log(A5);
+  console.log(A6);
+  console.log(A7);
+  console.log(A8);
+  console.log(A9);
+  console.log(A10);
+  console.log(A11);
+  console.log(A12);
+  console.log(A13);
+  console.log(A14);
+  console.log(A15);
+  console.log(A16);
+  console.log(F2);
+  console.log(F3);
+  console.log(F4);
+  console.log(F5);
+  console.log(F6);
+  console.log(F7);
+  console.log(F8);
+  console.log(F9);
+  console.log(F10);
+  console.log(F11);
+  console.log(F12);
+  console.log(F13);
+  console.log(F14);
+  console.log(F15);
+  console.log(F16);
 }
