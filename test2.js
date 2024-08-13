ba1638e8d5aa190ccb93db033bde0fb45b2ede32
sabareeswaran <151923697+devgoldenaxe@users.noreply.github.com>
2024-08-13 13:22:04 +0530

    update test calculation

---
+++ test2.js
@@ -0,0 +1,19 @@
+// Y1 = 114.16;
+// Daten3 = {
+//   G1: 4,
+// };
+// let data = {
+//   R5: 58.19,
+//   R6: 59.01,
+//   R7: 44.0,
+//   R8: 44.47,
+// };
+// let T6 = 4;
+// let T7 = 10;
+// let T8 = data[R5 + R6 + R7 + R8] / 4;
+// let G2 = (Y * Daten3.G1) / T6;
+// let G3 = G2 * 12;
+// let G4 = G3 * 5;
+// console.log(G2);
+// console.log(G3);
+// console.log(G4);
